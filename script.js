ac08c9e8d849aee6c8520eabf0e0f700916865e2
Montell Francis <montelldfrancis@gmail.com>
2023-08-16 03:39:16 -0400

    add event listener

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const h2 = document.createElement('h2');
-h2.textContent = 'This content added by JavaScript';
+// const h2 = document.createElement('h2');
+// h2.textContent = 'This content added by JavaScript';
 
-document.querySelector('body').appendChild(h2);
+// document.querySelector('body').appendChild(h2);
+
+const button = document.getElementById("button")
+button.addEventListener(
+    "mouseover",
+    (event) => {
+        event.target.style.color = "#1B9B96";
+        setTimeout(() => {
+            event.target.style.color = "";
+        }, 700);
+    },
+    false,
+);
